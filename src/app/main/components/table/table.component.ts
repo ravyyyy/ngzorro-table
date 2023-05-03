684750bc92a0ccb38ba7bea31d05fcd9ff5cd931
Efrim Dragos Alexandru <efrimdragos@gmail.com>
2023-05-03 20:05:57 +0300

    edit button complete

--- src/app/main/components/table/table.component.ts
+++ src/app/main/components/table/table.component.ts
@@ -13,6 +13,9 @@ export class TableComponent implements OnInit{
   booksList!: Book[];
   isModalVisible: boolean = false;
   bookForm!: FormGroup;
+  isEditModalVisible: boolean = false;
+  editBookForm!: FormGroup;
+  actualTitle!: string;
 
   constructor(
     private booksService: BooksService,
@@ -33,6 +36,15 @@ export class TableComponent implements OnInit{
       this.booksList = this.booksService.books;
       this.initializeForm();
       console.log(this.booksList);
+  }
+
+  initializeEditForm(book: Book): void {
+    this.editBookForm = new FormGroup({
+      title: new FormControl(book.title, [Validators.required]),
+      year: new FormControl(book.year, [Validators.required]),
+      author: new FormControl(book.author, [Validators.required]),
+      genre: new FormControl(book.genre, [Validators.required])
+    });
   }
 
   initializeForm(): void {
@@ -57,35 +69,61 @@ export class TableComponent implements OnInit{
   }
 
   edit(book: Book) {
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {
-        bookTitle: book.title,
-      },
-      queryParamsHandling: 'merge'
-    })
+    this.showEditModal();
+    this.actualTitle = book.title;
+    this.initializeEditForm(book);
   }
 
   handleOk(): void {
-    const newBook: Book = {
-      title: this.title.value,
-      year: this.year.value,
-      author: this.author.value,
-      genre: this.genre.value
-    };
+    if(this.bookForm.valid) {
+      const newBook: Book = {
+        title: this.title.value,
+        year: this.year.value,
+        author: this.author.value,
+        genre: this.genre.value
+      };
 
-    this.booksList.push(newBook);
-    this.booksService.booksListSubject.next(this.booksList);
+      this.booksList.push(newBook);
+      this.booksService.booksListSubject.next(this.booksList);
+    }
 
     this.isModalVisible = false;
+  }
+
+  handleEditOk(): void {
+    if (this.editBookForm.valid) {
+      const editedBook: Book = {
+        title: this.editBookForm.get('title')?.value,
+        year: this.editBookForm.get('year')?.value,
+        author: this.editBookForm.get('author')?.value,
+        genre: this.editBookForm.get('genre')?.value
+      };
+
+      const index = this.booksList.findIndex(book => book.title === this.actualTitle);
+    
+      if(index !== -1) {
+        this.booksList[index] = editedBook;
+        this.booksService.booksListSubject.next(this.booksList);
+      }
+    }
+
+    this.isEditModalVisible = false;
   }
 
   handleCancel(): void {
     this.isModalVisible = false;
   }
 
+  handleEditCancel(): void {
+    this.isEditModalVisible = false;
+  }
+
   showModal(): void {
     this.isModalVisible = true;
+  }
+
+  showEditModal(): void {
+    this.isEditModalVisible = true;
   }
 
   get title(): FormControl {
